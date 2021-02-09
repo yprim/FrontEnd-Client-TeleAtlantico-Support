b0056fb0fb67a952e0b90b9c2c7e1a57cc5cf0cb
yprim <priscipo@gmail.com>
2021-02-08 22:29:27 -0600

    models

---
+++ src/app/models/comment.model.ts
@@ -0,0 +1,27 @@
+export class User{
+
+    id: number;
+    name: string;
+    firstSurname:string;
+    secondSurname:string;
+    addres:string;
+    phone:string;
+    secondContact:string;
+    email:string;
+    password:string;
+    televison: 0 | 1;
+    mobilePhone: 0 | 1;
+    telephone : 0 | 1;
+    internet : 0 | 1;
+    creationDate: Date;
+    creationUser: Date;
+    updateDate?: Date;
+    updateUser?: string;
+
+
+
+
+
+
+
+}
---
+++ src/app/models/issue.model.ts
@@ -0,0 +1,11 @@
+export class Issue{
+
+    id: number=0;
+    description:string;
+    idIssue:number;
+    reportNumber: string;
+    creationDate: Date;
+    creationUser: Date;
+    updateDate?: Date;
+    updateUser?: string;
+}
